22d3eeeb9d30b6214d2915345329120a9d9c3e83
doodelidodo <dominic.meyer@me.com>
2022-01-01 11:56:26 +0100

    init
    test

---
+++ src/components/diary/Diary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Diary.css';
+
+export default function Diary(props) {
+    return(
+        <div className="card">
+           <div className="card-title">
+               <div className="card-day">13</div>
+               <div className="card-title">Ich bin ein Titel</div>
+           </div>
+        </div>
+    )
+}
