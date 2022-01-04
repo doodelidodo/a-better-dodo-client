5a1b6f19982adf22f7ad01691a9674592d6e4812
doodelidodo <dominic.meyer@me.com>
2022-01-04 22:47:29 +0100

    Card v1
    Card v1

--- src/components/diary/Diary.js
+++ src/components/diary/Diary.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './Diary.css';
+import image from  './macdo.jpeg'
 
 export default function Diary(props) {
     return(
         <div className="card">
-           <div className="card-title">
+           <div className="card-header">
                <div className="card-day">13</div>
-               <div className="card-title">Ich bin ein Titel</div>
+               <div className="card-title">
+                   <div>Ich bin ein Diary-Titel</div>
+               </div>
            </div>
+            <div>
+                <img className="card-img" src={image} />
+            </div>
+            <div className="card-body">
+                Das ist eine vorschau vom Text, der in einem Diary-Eintrag steht, er sollte nach einer gewissen länge abgeschnitten werden und mit 3 Punkten ergänzt werden. Somit ist es ein Teaser vom Modal, dass man aufmachen kann...
+            </div>
         </div>
     )
 }
